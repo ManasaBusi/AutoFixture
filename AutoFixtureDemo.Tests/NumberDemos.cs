0b05c50cb14b3546133f05f4b0079a3ad5292cc0
Busi, Manasa Deepthi <manasadeepthi.busi@swbc.com>
2024-06-15 19:14:00 -0500

    Add project files.

---
+++ AutoFixtureDemo.Tests/NumberDemos.cs
@@ -0,0 +1,21 @@
+﻿using DemoCode;
+
+namespace AutoFixtureDemo.Tests
+{
+    public class NumberDemos
+    {
+        [Fact]
+        public void Ints()
+        {
+            //Arrange
+            var sut = new IntCalculator();
+
+            //Act
+            sut.Subtract(1);
+
+            //Assert
+            Assert.True(sut.Value < 0);
+
+        }
+    }
+}
